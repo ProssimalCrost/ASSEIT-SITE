998a61fb38106fcb128940b4a3aa32e82e71d2c9
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-24 16:35:51 -0300

    Refactor carousel component: add background styling and update button layout

--- src/asseit-site/src/app/components/carousel.tsx
+++ src/asseit-site/src/app/components/carousel.tsx
@@ -72,23 +72,20 @@ export default function BackgroundCarousel() {
           transform: "translateY(-50%)",
         }}
       >
-      
-        <div className="flex flex-col gap-8">
+
+        <div className="flex flex-col gap-8 bg-gray-800 bg-opacity-60 p-2 rounded-lg mx-2 md:mx-20 lg:mx-40">
           <div className="justify-center items-center flex flex-col">
-           
             <h1 className="text-6xl font-extrabold text-white drop-shadow-lg mb-6">
                 Associação das Empresas Industriais de Timóteo
             </h1>
-           
+
           </div>
-          
           <div className="justify-center items-center flex flex-col">
-          
            <h1 className="text-2xl text-white text-lg mb-8 drop-shadow-md text-center">
               Unindo forças para o desenvolvimento industrial e comunitário de
               Timóteo.
            </h1>
-          
+  
            <Button>
              <button className="bg-blue-600 hover:bg-blue-600 text-white font-medium py-2 px-5 rounded-lg shadow-md hover:scale-105 transition-transform duration-200">Conheça a ASSEIT</button>
            </Button>
