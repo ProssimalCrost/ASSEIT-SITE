3cd4b50080984c0f66724ba00113689e65df1314
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-28 11:43:55 -0300

    Feat: alinhamento dos icones do navBar e estlização azul

--- src/asseit-site/src/app/components/navbar.tsx
+++ src/asseit-site/src/app/components/navbar.tsx
@@ -4,33 +4,37 @@ import Image from "next/image";
 import styled from "styled-components";
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
-import BusinessIcon from '@mui/icons-material/Business';
+import FactoryIcon from '@mui/icons-material/Factory';
 import PoolIcon from '@mui/icons-material/Pool';
 import CallIcon from '@mui/icons-material/Call';
 
 const StyledUl = styled("ul")`
   display: flex;
   gap: 3rem;
-  color: #000000ff;
+  color: #0e02b4ff;
+  list-style: none;
+  font-weight: 500;
+  font-size: 1.1rem;
+  align-items: center;
 `;
 
 export default function Navbar() {
   return (
     <header className="bg-white/60 backdrop-blur-md fixed top-0 w-full z-10 shadow-sm">
-      <nav className="max-w-xl mx-auto flex justify-between items-center p-3">
+      <nav className="max-w-lg mx-auto flex justify-between items-center p-3">
         <Image
             src="/images/logo.png"
             alt="Logo ASSEIT"
-            width={60}
-            height={60}
-            className="object-contain pr-5"
+            width={50}
+            height={50}
+            className="object-contain mr-10 pb-2"
           />
         <StyledUl>
-          <li><Link href="/"><HomeIcon fontSize="large"/></Link></li>
-          <li><Link href="/about"><InfoIcon fontSize="large"/></Link></li>
-          <li><Link href="/empresas"><BusinessIcon fontSize="large"/></Link></li>
-          <li><Link href="/estrutura"><PoolIcon fontSize="large"/></Link></li>
-          <li><Link href="/contato"><CallIcon fontSize="large"/></Link></li>
+          <li><Link href="/"><HomeIcon fontSize="large"/>Home</Link></li>
+          <li><Link href="/about"><InfoIcon fontSize="large"/>Sobre</Link></li>
+          <li><Link href="/empresas"><FactoryIcon fontSize="large"/><p>Empresas</p></Link></li>
+          <li><Link href="/estrutura"><PoolIcon fontSize="large"/><p>Estrutura</p></Link></li>
+          <li><Link href="/contato"><CallIcon fontSize="large"/><p>Contato</p></Link></li>
         </StyledUl>
       </nav>
     </header>
