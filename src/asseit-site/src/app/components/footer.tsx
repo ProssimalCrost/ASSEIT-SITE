8ea740a365622c6e0ed41b5e200b4deb798641e8
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-31 11:59:39 -0300

    Styled: Inclusão dos direitos de desenvolvimento

--- src/asseit-site/src/app/components/footer.tsx
+++ src/asseit-site/src/app/components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
     <footer className="bg-white/40 backdrop-blur-sm text-gray-00 text-center py-3 mt-12">
-      <p>© {new Date().getFullYear()} ASSEIT - Associação das Empresas Industriais de Timóteo</p>
+      <p>© {new Date().getFullYear()} Theylon - Associação das Empresas Industriais de Timóteo</p>
+      <p>Desenvolvido por: <a href="https://theylon.netlify.app/">Theylon</a></p>
     </footer>
   );
 }
