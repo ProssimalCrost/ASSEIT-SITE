526b1a9dacd7f99883e4486e570266c613d6c4c7
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-31 14:06:32 -0300

    feat: adição dos direitos reservados no elemento footer

--- src/asseit-site/src/app/components/footer.tsx
+++ src/asseit-site/src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-white/40 backdrop-blur-sm text-gray-00 text-center py-3 mt-12">
       <p>© {new Date().getFullYear()} Theylon - Associação das Empresas Industriais de Timóteo</p>
-      <p>Desenvolvido por: <a href="https://theylon.netlify.app/">Theylon</a></p>
+      <p>Desenvolvido por (link): <a href="https://theylon.netlify.app/">Theylon</a></p>
     </footer>
   );
 }
