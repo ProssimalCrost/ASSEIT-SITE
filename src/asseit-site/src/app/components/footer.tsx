24beb2cbb6460cc2dfb833053a84fa59380341b8
ProssimalCrost <theylon.silva@hotmail.com>
2025-11-14 16:24:57 -0300

    styled: harmonizing the color of the footer reducing its size.

--- src/asseit-site/src/app/components/footer.tsx
+++ src/asseit-site/src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-white/40 backdrop-blur-sm text-gray-00 text-center py-3 mt-12">
-      <p>© {new Date().getFullYear()} Theylon - Associação das Empresas Industriais de Timóteo</p>
+    <footer className="bg-white/30 backdrop-blur-sm text-gray-00 text-center py-1 mt-10">
+      <p>© {new Date().getFullYear()} Associação das Empresas Industriais de Timóteo</p>
       <p>Desenvolvido por (link): <a href="https://theylon.netlify.app/">Theylon</a></p>
     </footer>
   );
