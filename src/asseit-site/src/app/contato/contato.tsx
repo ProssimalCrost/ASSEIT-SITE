0f8704b97cb2f7a6cd27ef6b4ef8f8ac45d14f03
ProssimalCrost <theylon.silva@hotmail.com>
2026-02-17 09:10:46 -0300

    Styled: Adicionando sobraao balao contato, deixando o mesmo da cor branca; Feat: criando link para o whatzap na aba contato; Favicon na raiz de App

--- src/asseit-site/src/app/contato/contato.tsx
+++ src/asseit-site/src/app/contato/contato.tsx
@@ -1,21 +1,26 @@
 import Section from "../components/section";
 import Button from "../components/button";
 
+  const Whatsapp = () => {
+      const phone = "5531986584245";
+      const message = "Vamo codar juntos!";
+      const link = `https://api.whatsapp.com/send/?phone=55319986584245&text=Ola+diga+o+assunto%21&type=phone_number&app_absent=0`
+      window.open(link)
+    }
+
 export default function Contato() {
   return (
-
-      <main>
-      <div className="bg-blue-100 backdrop-blur-md rounded-2xl shadow-lg p-8 max-w-lg mx-auto">
-        <div><h1 className="text-2xl font-bold text-slate-900 text-center">Contato</h1></div>
-        <table className="w-full text-left border-separate border-spacing-y-3">
+      <div className="bg-white rounded-2xl shadow-lg shadow-slate-300 shadow-blue-100 p-10 max-w-4xl mx-auto justify-center items-center">
+        <div><h1 className="text-3xl font-bold text-slate-900 text-center">Contato</h1></div>
+        <table className="w-full text-center border-separate border-spacing-y-3">
           <thead>
-            <tr className="text-gray-700 border-b border-gray-200">
+            <tr className="text-black border-b border-gray-200 text-lg">
               <th className="py-2 px-3 font-semibold">Empresa</th>
               <th className="py-2 px-3 font-semibold">Contato</th>
             </tr>
           </thead>
           <tbody>
-            <tr className="hover:bg-gray-100 transition-colors">
+            <tr className="hover:bg-gray-100 transition-colors text-lg">
               <td className="py-2 px-3 text-gray-800">Secretaria do Clube</td>
               <td className="py-2 px-3 text-blue-600 font-medium">
                 <a href="tel:+553138491774" className="hover:underline">
@@ -23,7 +28,7 @@ export default function Contato() {
                 </a>
               </td>
             </tr>
-            <tr className="hover:bg-gray-100 transition-colors">
+            <tr className="hover:bg-gray-100 transition-colors text-lg">
               <td className="py-2 px-3 text-gray-800">Secretaria da Academia</td>
               <td className="py-2 px-3 text-blue-600 font-medium">
                 <a href="tel:+553138491774" className="hover:underline">
@@ -35,10 +40,11 @@ export default function Contato() {
         </table>
 
         <div className="text-center mt-6">
-          <Button>Enviar Mensagem</Button>
+          <Button onClick={Whatsapp}>
+            Enviar Mensagem
+          </Button>
         </div>
       </div>
-      </main>
 
   );
 }
